7d3119dc82db4947a03b96dd4a1de9ef195de940
Joel Tang <106056323+Jojrt92@users.noreply.github.com>
2022-12-31 15:11:41 +0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,12 @@ import plotly.express as px
 
 #----------------------------Import file---------------------------------#
 df = pickle.load(open("dataset/df_for_deployment.pkl","rb"))
-st.session_state.df = pd.DataFrame(df)
+df = pd.DataFrame(df)
 mapbox_access_token =  'pk.eyJ1Ijoiam9lbGpydCIsImEiOiJjbGJqaW12enYxM3M3M3hvZ2M1dzk3MThxIn0.xk3qkRogSnlc-wml2x63kQ'
 px.set_mapbox_access_token(mapbox_access_token)
-fig = px.scatter_mapbox(st.session_state.df, 
+
+
+fig = px.scatter_mapbox(df, 
                         lat="latitude", 
                         lon="longitude",     
                         color="zone_code", 
@@ -36,15 +38,17 @@ fig = px.scatter_mapbox(st.session_state.df,
 st.title("Map Of Revival #H21:fire::fire::fire:")
 
 plot_spot = st.empty()
-school = st.selectbox('Select school being prayed for revival',(df["school"]))
+school = st.selectbox('Select school being prayed for revival',(df["school"]), index = 0)
 number = st.number_input(label = 'Number of people', step = 1, min_value = 1)
 
+if 'df' not in st.session_state:
+    st.session_state.df = df
+    
 if st.button('Update revival map'):
-    #prayer_count(number,school)
     original_count = st.session_state.df.loc[school,"count"]
     original_count +=  number
     st.session_state.df.loc[school.upper(),['count']] = original_count
-    st.session_state.df = df
+    
     fig = px.scatter_mapbox(st.session_state.df, 
                         lat="latitude", 
                         lon="longitude",     
@@ -56,7 +60,7 @@ if st.button('Update revival map'):
                         size_max=10, 
                         zoom=9.2,
                        width=455, height=420)
-    
+
 
 #fig.update_traces(cluster=dict(enabled=True))
 with plot_spot:
